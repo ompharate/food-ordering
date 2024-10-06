31aa62a2ffada6bda128459d9077312ecfcdb838
om <ompharate31@gmail.com>
2024-10-06 22:35:56 +0530

    delete

--- app/cart/page.js
+++ app/cart/page.js
@@ -129,7 +129,7 @@ const Cart = () => {
                               d="M6 18 17.94 6M18 18 6.06 6"
                             />
                           </svg>
-                          Remove
+                          Remove delete
                         </button>
                       </div>
                     </div>
